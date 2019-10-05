451193dd748736a7253f08c8d0d11e387a78dda6
jupcan <juan.perea@alu.uclm.es>
2019-10-05 22:24:09 +0200

    creación listado citas

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,4 +1,9 @@
-function login($scope) {
-    $scope.text1 = '';
-    $scope.pwd1 = '';
-  }
+function add($scope) {
+    $scope.items = ['cita1', 'cita2', 'cita3'];
+    $scope.AddItem = function() {
+        $scope.items.push($scope.newItem);
+    };
+    $scope.RemoveItem = function(item) {
+        $scope.items.splice($scope.items.indexOf(item), 1);
+    };
+};
