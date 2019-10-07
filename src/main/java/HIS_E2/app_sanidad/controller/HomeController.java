16d5a65ff5adc5d6e3d0fd6f126b215d8ea6b014
MiguelMMA <43533278+MiguelMMA@users.noreply.github.com>
2019-10-07 12:33:24 +0200

    Spring angular login (#15)
    * Configuración de notificaciones con Slack, unido con GitHub y Travis CI.
    
    * Arreglo de espacios en el archivo .travis.yml para la integración
    continua.
    
    * Cambio de directorio raíz para facilidad de comunicación con Heroku.
    
    * Java actualizado a 1.8 para integracion de spring
    
    * Añadida la clase principal al pom
    
    * SpringBoot funcionando con html de prueba
    
    * Añadida clase principal al POM
    
    * Login simple creado, funcionando con spring
    
    * Integración citas y login
    
    * URL cambiada a la aplicación de despliegue

--- src/main/java/HIS_E2/app_sanidad/controller/HomeController.java
+++ src/main/java/HIS_E2/app_sanidad/controller/HomeController.java
@@ -2,13 +2,14 @@ package HIS_E2.app_sanidad.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
 	
 	@RequestMapping("/home")
 	public String home() {
-		return "index";
+		return "index.html";
 	}
 
 }
