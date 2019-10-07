b633f042c193adfe9cc139fa57a07eb450573001
MiguelMMA <43533278+MiguelMMA@users.noreply.github.com>
2019-10-07 18:26:46 +0200

    Spring angular login (#17)
    * Configuración de notificaciones con Slack, unido con GitHub y Travis CI.
    
    * Arreglo de espacios en el archivo .travis.yml para la integración
    continua.
    
    * Cambio de directorio raíz para facilidad de comunicación con Heroku.
    
    * Java actualizado a 1.8 para integracion de spring
    
    * Añadida la clase principal al pom
    
    * SpringBoot funcionando con html de prueba
    
    * Añadida clase principal al POM
    
    * Login simple creado, funcionando con spring
    
    * Integración citas y login
    
    * URL cambiada a la aplicación de despliegue
    
    * Index.html estandarizado para los tests
    
    * Arreglado errores de checkstyle y PMD
    
    * Añadido logo SESCAM
    
    * Añadidos nombres a Index para los tests

--- src/main/java/HIS_E2/app_sanidad/controller/CitasController.java
+++ src/main/java/HIS_E2/app_sanidad/controller/CitasController.java
@@ -4,9 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+/**Clase controller de la página de citas.
+ * @author Miguel
+ */
 @Controller
 public class CitasController {
 
+	/**Controlador mapeo de la página de citas.
+	 * @return Devuelve el html de la página de citas.
+	 */
 	@RequestMapping(value = "/citas", method = RequestMethod.GET)
 	public String getCitas() {
 		return "views/citas.html";
