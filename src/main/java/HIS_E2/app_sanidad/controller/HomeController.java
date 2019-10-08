98e3be565485fcb74b62b64d37f91fec9219799c
Eduardo Rodríguez Barbosa <43600766+Edulaen@users.noreply.github.com>
2019-10-08 12:19:38 +0200

    Versión 1.0.0 estable (#19)
    * Configuración de notificaciones con Slack, unido con GitHub y Travis CI.
    
    * Arreglo de espacios en el archivo .travis.yml para la integración
    continua.
    
    * Cambio de directorio raíz para facilidad de comunicación con Heroku.
    
    * Java actualizado a 1.8 para integracion de spring
    
    * Añadida la clase principal al pom
    
    * SpringBoot funcionando con html de prueba
    
    * Añadida clase principal al POM
    
    * Login simple creado, funcionando con spring
    
    * Integración citas y login
    
    * URL cambiada a la aplicación de despliegue
    
    * Index.html estandarizado para los tests
    
    * Cucumber tests (#16)
    
    * Configuración de notificaciones con Slack, unido con GitHub y Travis CI.
    
    * Arreglo de espacios en el archivo .travis.yml para la integración
    continua.
    
    * Cambio de directorio raíz para facilidad de comunicación con Heroku.
    
    * Java actualizado a 1.8 para integracion de spring
    
    * Añadida la clase principal al pom
    
    * Deployment quitado del .travis para futura implementación.
    
    * SpringBoot funcionando con html de prueba
    
    * Añadida clase principal al POM
    
    * Deployment de heroku en .travis.yml
    
    * Index html en carpeta views
    
    * Diseño y creación de tests
    
    Diseño en src/test/resources/HIS_E2/app_sanidad
    Implementación en src/test/java/HIS_E2.app_sanidad
    
    * Actualización driver Chrome
    
    * Cucumber Tests actualización para que travis lo acepte
    
    * Actualización tests
    
    * Eliminada redundancia de springboot-tests en pom.xml
    
    * Cambiar forma de ejecutar travis para que no de error con los tests
    
    * eliminar instancia de driver despues de usarlo
    
    * Arreglado error en el que HTTP salía 2 veces en una URL
    
    * Primer test descomentado
    
    * Arreglado errores de checkstyle y PMD
    
    * Añadido logo SESCAM
    
    * Añadidos nombres a Index para los tests
    
    * Cambio http tests

--- src/main/java/HIS_E2/app_sanidad/controller/HomeController.java
+++ src/main/java/HIS_E2/app_sanidad/controller/HomeController.java
@@ -2,11 +2,15 @@ package HIS_E2.app_sanidad.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
+/**Clase controladora del index de la página.
+ * @author Miguel
+ */
 @Controller
 public class HomeController {
-	
+	/**Controla el mapeo de la página de incio.
+	 * @return Devuelve el html del index.
+	 */
 	@RequestMapping("/home")
 	public String home() {
 		return "index.html";
