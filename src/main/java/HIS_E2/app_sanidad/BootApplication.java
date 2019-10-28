be42b3fb02fdb7c9c4c63c9722ab297604534316
Miguel <Miguel@172.19.215.109>
2019-10-28 12:15:10 +0100

    Añadido enbale Mongo repo

--- src/main/java/HIS_E2/app_sanidad/BootApplication.java
+++ src/main/java/HIS_E2/app_sanidad/BootApplication.java
@@ -2,11 +2,14 @@ package HIS_E2.app_sanidad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
 /**Clase Lanzadora del SpringBoot.
  * @author Miguel
  *
  */
 @SpringBootApplication
+@EnableMongoRepositories
 public class BootApplication {
 
 	/**Main de la clase lanzadora de SpringBoot.
