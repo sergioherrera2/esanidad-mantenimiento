b773939dda032a5dfff244392b4ea826be14a77b
Eduardo Rodríguez Barbosa <Eduardo.Rodriguez10@alu.uclm.es>
2019-11-04 13:36:02 +0100

    URLs cambiadas para despliegue Heroku.

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -2,7 +2,7 @@ function validateLogin() {
     var usuario = document.getElementById("username").value;
     var contraseña = document.getElementById("password").value;
 
-        var recurso = "http://localhost:8080/autenticar";
+        var recurso = "https://app-sanidad.herokuapp.com/autenticar";
         var data = {
       dni: usuario,
       pass: contraseña,
@@ -22,11 +22,11 @@ function validateLogin() {
         .done(function( data, textStatus, jqXHR ) {
           console.log(data.type+"HOLA");
           if ( data.type=="OK" ) {
-             setTimeout( location.href='http://localhost:8080/paciente',10000);
+             setTimeout( location.href='https://app-sanidad.herokuapp.com/paciente',10000);
            
           }else{
           	if(usuario == "medico") {
-          	setTimeout( location.href='http://localhost:8080/indexD',10000);
+          	setTimeout( location.href='https://app-sanidad.herokuapp.com/indexD',10000);
           	}
             window.alert(data.message)
             console.log(data.message)
