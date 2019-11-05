dc9d8d768d07a2e1a225e973c150e4b673fb3ba4
Antonio Pulido Hernández <Antonio.Pulido@alu.uclm.es>
2019-11-05 22:27:55 +0100

    Calidad

--- src/main/java/HIS_E2/app_sanidad/controller/WebController.java
+++ src/main/java/HIS_E2/app_sanidad/controller/WebController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import HIS_E2.app_sanidad.model.Cita;
