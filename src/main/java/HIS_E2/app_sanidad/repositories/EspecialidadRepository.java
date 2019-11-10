04bf253df35225b861d5bbb6c8203b9dc652e835
Miguel <Miguel@172.19.215.109>
2019-11-10 12:11:37 +0100

    Control solapamiento de citas

--- src/main/java/HIS_E2/app_sanidad/repositories/EspecialidadRepository.java
+++ src/main/java/HIS_E2/app_sanidad/repositories/EspecialidadRepository.java
@@ -1,6 +1,9 @@
 package HIS_E2.app_sanidad.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import HIS_E2.app_sanidad.model.Especialidad;
@@ -8,4 +11,6 @@ import HIS_E2.app_sanidad.model.Especialidad;
 @Repository
 public interface EspecialidadRepository extends MongoRepository<Especialidad, String> {
 
+	@Query(value = "{ 'nombreEspecialidad' : ?0}")
+	Especialidad findByEspecialidad(String nombreEspecialidad);
 }
