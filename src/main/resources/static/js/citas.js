fd7ecd6b62378bf24034eef16973d3dc88d38f29
ThElementh <ThElementh@DESKTOP-64AT9HQ>
2019-11-11 18:17:32 +0100

    añadida peticion de citas al servidor

---
+++ src/main/resources/static/js/citas.js
@@ -0,0 +1,42 @@
+function pedirCita() {
+    var fecha = document.getElementById("fecha").value;
+    var especialidad = document.getElementById("especialidad").value;
+    var DNI = JSON.parse(sessionStorage.getItem("data"));
+
+    var recurso = "http://localhost:8080/pedirCita";
+        var data = {
+            type: "cita",
+            dniPaciente: DNI,
+            fecha : fecha,
+            especialidad : especialidad
+        };
+        data = JSON.stringify(data);
+
+        setTimeout($.ajax({
+                url: recurso,
+                type: "POST",
+                data: data,
+                xhrFields: {
+                    withCredentials: true
+                },
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            })
+        .done(function(data, textStatus, jqXHR) {
+            console.log(data.type);
+            if (data.type == "OK") {
+            	
+                setTimeout(location.href = 'http://localhost:8080/paciente', 10000);
+
+            } else {
+                if (data.type="error") {
+                    alert("Error cita ya existente, contacte con el servicio de soporte.");
+                }
+                
+
+            }
+
+        }), 10000);
+        
+    }
