f18e1d33ea28173b22af2dc572aac33087d8d824
jupcan <juan.perea@alu.uclm.es>
2019-11-25 21:24:35 +0100

    javascript médico-paciente

--- src/main/resources/static/js/medicoPaciente.js
+++ src/main/resources/static/js/medicoPaciente.js
@@ -1,71 +1,34 @@
-var DNI = JSON.parse(sessionStorage.getItem("data"));
-var recurso = "http://localhost:8080/consultaEspecialidades";
-var datosNombre = [];
+function crearMedicoPaciente() {
+    var medico = document.getElementById("dniMedico").value;
+    var paciente = document.getElementById("dniPaciente").value;
 
-var data = {
-    dni : DNI,
-};
-data = JSON.stringify(data);
-setTimeout($.ajax({
-    url : recurso,
-    type : "POST",
-    data : data,
-    xhrFields : {
-        withCredentials : true
-    },
-    headers : {
-        'Content-Type' : 'application/json'
-    },
-}).done(function(data, textStatus, jqXHR) {
-    if (data.type == "OK") {
-        for (var i = 0; i < (data.numero); i++) {
-            datosNombre[i] = data['nombreEspecialidad' + i];
+    var recurso = "http://localhost:8080/crearMedicoPaciente";
+    var data = {
+        type: "medico-paciente",
+        dniMedico: medico,
+        dniPaciente: paciente
+    };
+    data = JSON.stringify(data);
+    setTimeout($.ajax({
+        url: recurso,
+        type: "POST",
+        data: data,
+        xhrFields: {
+            withCredentials: true
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+    .done(function(data, textStatus, jqXHR) {
+        console.log(data.type);
+        console.log(data.message);
+        if (data.type == "OK") {
+            setTimeout(location.href = 'http://localhost:8080/gestor', 10000);
+        } else {
+            if (data.type="error") {
+                alert("Error al crear la relación médico-paciente, contacte con el servicio de soporte.");
+            }
         }
-        mostrarEspecialidades(datosNombre);
-    }
-}), 10000);
-
-function mostrarEspecialidades(datosNombre) {
-    var select_especialidades = "";
-
-    for (var i = 0; i < datosNombre.length; i++) {
-        select_especialidades += '<option>' + datosNombre[i] + '</option>';
-    }
-    $("#especialidad").append(select_especialidades);
-}
-
-function crearMedico() {
-  var dni = document.getElementById("dni").value;
-  var especialidad = document.getElementById("especialidad").value;
-  var DNI = JSON.parse(sessionStorage.getItem("data"));
-
-  var recurso = "http://localhost:8080/crearMedico";
-  var data = {
-      type: "medico",
-      dni: dni,
-      especialidad: especialidad
-  };
-  data = JSON.stringify(data);
-  setTimeout($.ajax({
-    url: recurso,
-    type: "POST",
-    data: data,
-    xhrFields: {
-        withCredentials: true
-    },
-    headers: {
-        'Content-Type': 'application/json'
-    },
-  })
-  .done(function(data, textStatus, jqXHR) {
-    console.log(data.type);
-    console.log(data.message);
-    if (data.type == "OK") {
-        setTimeout(location.href = 'http://localhost:8080/gestor', 10000);
-    } else {
-        if (data.type="error") {
-            alert("Error al crear el médico, contacte con el servicio de soporte.");
-        }
-    }
-  }), 10000);
+    }), 10000);
 }
