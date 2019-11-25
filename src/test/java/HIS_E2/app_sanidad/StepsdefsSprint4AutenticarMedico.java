f034601fcf532a8fae52020bcbbdfb942895704b
Antonio Pulido Hernández <Antonio.Pulido@alu.uclm.es>
2019-11-25 18:52:06 +0100

    Travis prueba

--- src/test/java/HIS_E2/app_sanidad/StepsdefsSprint4AutenticarMedico.java
+++ src/test/java/HIS_E2/app_sanidad/StepsdefsSprint4AutenticarMedico.java
@@ -126,13 +126,13 @@ public class StepsdefsSprint4AutenticarMedico {
 		}
 
 	    try {
-		  //  System.setProperty("webdriver.gecko.driver", "src/test/resources/HIS_E2/app_sanidad/geckodriver");					
+		    System.setProperty("webdriver.gecko.driver", "src/test/resources/HIS_E2/app_sanidad/geckodriver");					
 
 		    DesiredCapabilities dc = new DesiredCapabilities();
 		    dc.setCapability(CapabilityType.UNEXPECTED_ALERT_BEHAVIOUR, UnexpectedAlertBehaviour.IGNORE);
-		    System.setProperty("webdriver.chrome.driver", "src/test/resources/HIS_E2/app_sanidad/chromedriver.exe");
-		     driver = new ChromeDriver();
-		    //driver = new FirefoxDriver();
+		    //System.setProperty("webdriver.chrome.driver", "src/test/resources/HIS_E2/app_sanidad/chromedriver.exe");
+		    // driver = new ChromeDriver();
+		    driver = new FirefoxDriver();
 		    System.out.println(driver);
 		    driver.manage().window().maximize();
 		    
