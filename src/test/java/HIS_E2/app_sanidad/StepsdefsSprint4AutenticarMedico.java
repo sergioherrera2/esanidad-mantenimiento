03727adad64b048f148dae798d28c7d9f1153670
Antonio Pulido Hernández <Antonio.Pulido@alu.uclm.es>
2019-11-25 19:14:46 +0100

    travis pruebas

--- src/test/java/HIS_E2/app_sanidad/StepsdefsSprint4AutenticarMedico.java
+++ src/test/java/HIS_E2/app_sanidad/StepsdefsSprint4AutenticarMedico.java
@@ -132,7 +132,7 @@ public class StepsdefsSprint4AutenticarMedico {
 		    dc.setCapability(CapabilityType.UNEXPECTED_ALERT_BEHAVIOUR, UnexpectedAlertBehaviour.IGNORE);
 		    //Syçstm.setProperty("webdriver.chrome.driver", "src/test/resources/HIS_E2/app_sanidad/chromedriver.exe");
 		    // driver = new ChromeDriver();
-		    
+		    System.out.println("TRAVIS PRUEBAAA1");
 		    driver = new FirefoxDriver();
 		    System.out.println(driver);
 		    driver.manage().window().maximize();
